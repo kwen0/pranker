399a3657179b38c9aaae228b42331edb2a75c551
Chuma Okoro <chuma.okoro76@gmail.com>
2022-01-16 19:26:01 -0500

    use spring boot to intialize project

---
+++ src/main/java/mvp/Pranker/PrankerApplication.java
@@ -0,0 +1,13 @@
+package mvp.Pranker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrankerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrankerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mvp/Pranker/PrankerApplicationTests.java
@@ -0,0 +1,13 @@
+package mvp.Pranker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrankerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
